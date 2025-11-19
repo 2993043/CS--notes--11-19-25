0b8b4638c0bf81551d0f8421ef429037dfa69c12
2993043 <2993043@stocktonusd.org>
2025-11-19 18:06:37 +0000

    notes, hw

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
+answer = 0
+print("This is your answer at the start: "+str(answer))
+for i in range (15):
+    #action
+    answer = answer
 
+    print("this is the final asnwer: "+str(answer))
+
+
+(pi)()
